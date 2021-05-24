5b606e9ced92d5168aaee1ac230eba243cb3bc22
Gustavo Belbruno <gustavobelbruno@gmail.com>
2021-05-24 14:43:59 -0300

    updated ble config

--- main/ble_beacon_scanner.c
+++ main/ble_beacon_scanner.c
@@ -29,12 +29,12 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *par
 
 
 static esp_ble_scan_params_t ble_scan_params = {
-    .scan_type              = BLE_SCAN_TYPE_ACTIVE,
+    .scan_type              = BLE_SCAN_TYPE_PASSIVE,
     .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
     .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
     .scan_interval          = 0x06E1,
     .scan_window            = 0x06E0,
-    .scan_duplicate         = BLE_SCAN_DUPLICATE_DISABLE
+    .scan_duplicate         = BLE_SCAN_DUPLICATE_ENABLE
 };
 
 
